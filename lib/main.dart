365691fc9e4a379f0cef12c51baaab0ea88b98d6
yordanos <yordanosfikadu2017@gmail.com>
2023-10-12 19:53:21 +0300

    rotatedbox widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui/build_flow.dart';
 import 'package:flutter_ui/layout_widget.dart';
+import 'package:flutter_ui/paint_and_effect.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: LayoutWidget(),
+    home: PaintAndEffect(),
   ));
 }
