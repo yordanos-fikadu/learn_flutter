0d523f5a8d2b2d1e96b81cbb817a66b51a8b0106
yordanos <yordanosfikadu2017@gmail.com>
2023-10-12 09:56:34 +0300

    expand widget

--- lib/layout_widget.dart
+++ lib/layout_widget.dart
@@ -16,9 +16,9 @@ class _LayoutWidgetState extends State<LayoutWidget> {
       appBar: AppBar(
         title: Text('Widgets'),
       ),
-      backgroundColor: Color.fromARGB(255, 56, 8, 128),
+      backgroundColor: Colors.white,
       body: Center(
-        child: _buildAspectRatio(),
+        child: _buildExpand(),
       ),
     );
   }
@@ -31,14 +31,70 @@ class _LayoutWidgetState extends State<LayoutWidget> {
   }
 
   Widget _buildAspectRatio() {
-    return Align(
+    return Container(
+      color: Colors.blue,
+      alignment: Alignment.center,
+      width: 100,
+      height: 100,
       child: AspectRatio(
-        aspectRatio: 0.5,
+        aspectRatio: 16 / 9,
         child: Container(
-          height: 100,
-          color: Colors.red,
+          color: Colors.green,
         ),
       ),
+    );
+  }
+
+  Widget _buildBaseLine() {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.red,
+      child: Baseline(
+          baseline: 90,
+          baselineType: TextBaseline.alphabetic,
+          child: Text('Baseline')),
+    );
+  }
+
+  Widget _buildConstraibedBox() {
+    return Center(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 80),
+        child: Column(children: [
+          Text('Constrained Box'),
+          OutlinedButton(onPressed: () => null, child: Text('Button'))
+        ]),
+      ),
+    );
+  }
+
+  Widget _buildExpand() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          flex: 3,
+          child: Container(
+            color: Colors.red,
+            height: 20,
+            child: Text('1'),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Colors.yellow,
+            height: 20,
+            child: Text('1'),
+          ),
+        ),
+        Container(
+          color: Colors.green,
+          height: 20,
+          child: Text('1'),
+        )
+      ],
     );
   }
 }
